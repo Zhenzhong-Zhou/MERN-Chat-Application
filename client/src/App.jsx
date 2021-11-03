0196ff21ba20b3a3ffffa87b38290cc3edde2c0f
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-11-03 14:33:06 -0600

    Add auth into app, but sockect cannot pass

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import {io} from "socket.io-client";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+import {useSelector} from "react-redux";
 import {Home, Login, NotFound, Register, Room} from "./pages";
 
 const App = ()  => {
 	const socket = io(process.env.REACT_APP_LOCAL_URL);
+	const auth = useSelector(state => state.auth.currentUser);
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route exact path={"/register"} component={() => <Register socket={socket}/>}/>
-				<Route exact path={"/login"} component={() => <Login/>}/>
+				<Route exact path={"/register"} component={() => (auth ? <Redirect to={"/"}/> : <Register/>)}/>
+				<Route exact path={"/login"} component={() => (auth ? <Redirect to={"/"}/> : <Login/>)}/>
 				<Route exact path={"/"} component={Home}/>
 				<Route exact path={"/room"} component={Room} socket={socket}/>
 				<Route exact path={""} component={NotFound}/>
