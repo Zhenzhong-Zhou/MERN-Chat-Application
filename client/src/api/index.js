c27c7f1f92d538e0650c6ee93cfc313f1f7eabca
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-11-03 13:30:41 -0600

    Add login redux action

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,27 @@
 import axios from "axios";
+import {loginFailure, loginStart, loginSuccess} from "../redux/auth";
 
 export const axiosUser = axios.create({
 	baseURL: process.env.REACT_APP_BASE_LOCAL_URL
 });
+
+const API = axios.create({
+	baseURL: process.env.REACT_APP_BASE_LOCAL_URL
+});
+
+API.interceptors.request.use((req) => {
+	if (localStorage.getItem("persist:root")) {
+		req.headers.authorization = `Bearer ${JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken}`;
+	}
+	return req;
+});
+
+export const login = async (dispatch, user) => {
+	dispatch(loginStart());
+	try {
+		const {data} = await API.post("auth/login", user);
+		dispatch(loginSuccess(data));
+	} catch (error) {
+		dispatch(loginFailure());
+	}
+};
