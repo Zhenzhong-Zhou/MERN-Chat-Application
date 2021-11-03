14eabc7457a0d48856da363d74d2ecd088419296
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-11-03 16:54:01 -0600

    Clean code

--- server/handlers/joinRoom.js
+++ server/handlers/joinRoom.js
@@ -3,9 +3,8 @@ import User from "../models/user.js";
 export const joinRoom = (io, socket) => {
 	console.log(`${socket.id} has connected!`);
 	socket.on("join_room", async (user) => {
-		console.log(user)
-		 await User.findById(user._id);
-			// socket.join(data);
+		await User.findById(user._id);
+		// socket.join(data);
 		// console.log(`User with ID: ${socket.id} joined room: ${data}`)
 	});
 };
