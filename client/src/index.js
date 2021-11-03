b37a0d408bd5f231287a0b3bf4b65ab6f2dc7487
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-11-03 12:50:28 -0600

    Add redux into client app, and add some dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,12 @@
 import reactDOM from "react-dom";
+import {Provider} from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
 import App from "./App";
+import {store, persi_store} from "./reducers/index";
 
-reactDOM.render(<App/>, document.getElementById("root"));
+reactDOM.render(
+	<Provider store={store}>
+		<PersistGate loading={"null"} persistor={persi_store}>
+			<App />
+		</PersistGate>
+	</Provider>, document.getElementById("root"));
