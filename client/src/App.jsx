3c343705bcde233b7e3ae7e32c88e5bc439e9414
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-11-02 22:30:37 -0600

    Initial register page into client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import {io} from "socket.io-client";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import {Home, Login} from "./pages";
+import {Home, Login, Register} from "./pages";
 
 const App = ()  => {
 	const socket = io(process.env.REACT_APP_LOCAL_URL);
 	return (
 		<BrowserRouter>
 			<Switch>
+				<Route exact path={"/register"} component={() => <Register socket={socket}/>}/>
 				<Route exact path={"/login"} component={() => <Login socket={socket}/>}/>
 				<Route exact path={"/"} component={Home}/>
 			</Switch>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,13 @@
+import {Link} from "react-router-dom";
+
 const Home = () => {
 	return (
-		<div>Home</div>
+		<div>
+			<h1>Home</h1>
+			<Link to={"/register"}><span>Register</span></Link>
+			<br/>
+			<Link to={"/login"}><span>Login</span></Link>
+		</div>
 	);
 };
 
