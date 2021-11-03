884a54352042dc7635c23cce7cf8875a12939cb2
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-11-03 14:06:51 -0600

    Initial room page into client app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import {Home, Login, NotFound, Register} from "./pages";
+import {Home, Login, NotFound, Register, Room} from "./pages";
 
 const App = ()  => {
 	const socket = io(process.env.REACT_APP_LOCAL_URL);
@@ -8,8 +8,9 @@ const App = ()  => {
 		<BrowserRouter>
 			<Switch>
 				<Route exact path={"/register"} component={() => <Register socket={socket}/>}/>
-				<Route exact path={"/login"} component={() => <Login socket={socket}/>}/>
+				<Route exact path={"/login"} component={() => <Login/>}/>
 				<Route exact path={"/"} component={Home}/>
+				<Route exact path={"/room"} component={Room} socket={socket}/>
 				<Route exact path={""} component={NotFound}/>
 			</Switch>
 		</BrowserRouter>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,5 @@
 export {default as Register} from "./Register/Register";
 export {default as Login} from "./Login/Login";
 export {default as Home} from "./Home/Home";
+export {default as Room} from "./Room/Room";
 export {default as NotFound} from "./NotFound/NotFound";
